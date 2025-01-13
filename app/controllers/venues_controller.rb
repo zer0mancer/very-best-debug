921d1f15748e38c956c51c0c904890e1e120f5e7
Jim Vongsouvanh <95146291+zer0mancer@users.noreply.github.com>
2025-01-13 04:56:33 +0000

    update venue added

--- app/controllers/venues_controller.rb
+++ app/controllers/venues_controller.rb
@@ -28,13 +28,15 @@ class VenuesController < ApplicationController
   def update
     the_id = params.fetch("venue_id")
 
-    @venue = Venue.where({ :id => the_id })
-    venue.address = params.fetch("query_address")
-    venue.name = params.fetch("Query_name")
-    venue.neighborhood = params.fetch("query_neighborhood")
-    venue.save
+    the_venue = Venue.where({ :id => the_id })
+
+    @venue = the_venue.at(0)
+    @venue.address = params.fetch("address")
+    @venue.name = params.fetch("name")
+    @venue.neighborhood = params.fetch("neighborhood")
+    @venue.save
     
-    redirect_to("/venues/#{venue.id}")
+    redirect_to("/venues/#{@venue.id}")
   end
 
   def destroy
--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
   get("/venues", { :controller => "venues", :action => "index" })
   post("/insert_venue_record", { :controller => "venues", :action => "create" })
   get("/venues/:venue_id", { :controller => "venues", :action => "show" })
-  post("/update_venue/:the_id", { :controller => "venues", :action => "update" })
+  post("/update_venue/:venue_id", { :controller => "venues", :action => "update" })
   get("/delete_venue/:id_to_delete", { :controller => "venues", :action => "destroy" })
   
   post("/insert_comment_record", { :controller => "comments", :action => "create" })
